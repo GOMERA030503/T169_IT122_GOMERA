c39e7ca60317612a5ba439a43ba3e71155c7a74b
GOMERA030503 <115513375+GOMERA030503@users.noreply.github.com>
2022-10-11 14:37:35 +0800

    Add files via upload

---
+++ AVERAGE.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int ESP,TLE,SCIANCE,MATH,ENGLISH;
+float Average;
+Printf("input the grade of ESP: ");:
+Scanf("%d",ESP)
+Printf("input the grade of TLE: ");
+Scanf("%d",TLE)
+Printf("input the grade of SCIENCE: ");
+Scanf("%d",SCIENCE)
+Printf("input the grade of MATH: ");
+scanf("%d",math)
+Printf("input the average of ENGLISH: ");
+Scanf("%d",ENGLISH)
+Average=(float(ESP+TLE+SCIENCE+MATH+ENLISH)/5);
+printf ("the average is:%2f.average");
+return 0
+
+}
